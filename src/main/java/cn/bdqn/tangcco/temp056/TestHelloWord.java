fc877134348502a4a6ffbd03305c43b8b9f956fd
zhounbing1 <1071594554@qq.com>
2017-08-04 09:49:14 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/temp056/TestHelloWord.java
@@ -0,0 +1,12 @@
+package cn.bdqn.tangcco.temp056;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+public class TestHelloWord {
+
+    public static void main(String[] args) {
+        System.out.println(("你是一个啥子"));
+    }
+
+}
